656cf7947749ee39535d33bb15aee238bc4a1e81
Ollo <ollo@ollo.mobil>
2020-10-17 17:37:24 +0200

    Added settings for night mode

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,11 @@ HomieNode lampNode("lamp", "Lamp", "WhiteLED");
 HomieNode motion("motion", "motion", "Motion detected");
 
 HomieSetting<long> ledAmount("leds", "Amount of LEDs (of type WS2812); Range 1 to 2047");
+HomieSetting<bool> motionActivation("motionactivation", "Activate light on motion");
+HomieSetting<const char*> dayColor("colorDay", "color to show at day");
+HomieSetting<const char*> nightColor("nightColor", "color to show at night");
+HomieSetting<long> nightStartHour("nightstart", "Hour when night starts (0-23)");
+HomieSetting<long> nightEndHour("nightend", "Hour when night ends (0-23)");
 
 bool mHomieConfigured = false;
 unsigned long mLastLedChanges = 0U;
