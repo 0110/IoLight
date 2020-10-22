3ca5fdeb567807740edef58de18dabf5817a1b5e
Ollo <Ollo@host>
2020-10-22 21:21:59 +0200

    lower case hex added

--- test/test_desktop/test_colorutil.cpp
+++ test/test_desktop/test_colorutil.cpp
@@ -40,10 +40,26 @@ void test_RRGGBB(void) {
     TEST_ASSERT_EQUAL(0x000000FFU, CALL_COLOREXTARCT("#0000FF"));
 }
 
+
+/**
+ * @brief Test hex values, describing red, green blue
+ * 
+ */
+void test_rrggbb(void) {
+
+                                                    /* rr gg bb */
+    TEST_ASSERT_EQUAL(0x00000000U, CALL_COLOREXTARCT("#000000")); 
+    TEST_ASSERT_EQUAL(0x00FF0000U, CALL_COLOREXTARCT("#ff0000"));
+    TEST_ASSERT_EQUAL(0x0000FF00U, CALL_COLOREXTARCT("#00ff00"));
+    TEST_ASSERT_EQUAL(0x000000FFU, CALL_COLOREXTARCT("#0000ff"));
+    TEST_ASSERT_EQUAL(0x00123456U, CALL_COLOREXTARCT("#123456"));
+}
+
 int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_colorConversion);
     RUN_TEST(test_RRGGBB);
+    RUN_TEST(test_rrggbb);
     UNITY_END();
 
     return 0;
